23cc6bf2a0a2e772db1103eeda75fbd399a4ed8d
The Programming Laptop <Robert Steele@Skunkworks1.(none)>
2012-02-23 20:06:43 -0800

    Removed obselete camera code. TODO: Add the socket, and create a callback for packet reception in the server1180 class
    Signed-off-by: Westin Miller <equinoxscripts@gmail.com>

--- FRC2012/Asbestos2012/C1983GlyphCamera.h
+++ FRC2012/Asbestos2012/C1983GlyphCamera.h
@@ -7,21 +7,16 @@
 #ifndef __C1983GLYPHCAMERA_H
 #define __C1983GLYPHCAMERA_H
 
-#include "WPILib.h"
-#include "Vision/BinaryImage.h"
-#include "Vision/RGBImage.h"
-
 class C1983GlyphCamera
 {
 private:
-	AxisCamera * camera;
-	BinaryImage * image;
-	RGBImage * camBuffer;
+	double currentYaw = -1;
+	double currentDepth = -1;
 public:
 	C1983GlyphCamera();
-
-	//Get the location of the current set of glyphs
-	void doTracking();
+	void processPacket();
+	double getCurrentYaw();
+	double getCurrentDepth();
 };
 #endif
 
